f2c9011804866df9e368da8b22d6b4b72c63afb2
viver <kaicope004@gmail.com>
2023-08-15 19:53:07 +0100

    Update for Heroku

--- config/database.js
+++ config/database.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log('Connected to MongoDB...');
+  }).catch((err) => {
+    console.error('Could not connect to MongoDB...', err);
+  });
 
 const db = mongoose.connection;
 	
